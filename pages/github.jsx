64414818376a208b55939ae43b435d88be7cca48
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-04-16 18:03:15 +0530

    first commit

--- pages/github.jsx
+++ pages/github.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import GitHubCalendar from "react-github-calendar";
 import RepoCard from "../components/RepoCard";
 import styles from "../styles/GithubPage.module.css";
-
+//updated
 const GithubPage = ({ repos, user }) => {
   const theme = {
     level0: "#161B22",
